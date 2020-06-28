7b8e595e29ccd97b64aefa761263e2398b893a66
yuxinsheng <yuxinsheng_mail@163.com>
2020-06-28 10:45:09 +0800

    update

--- src/main/java/com/yuxinsheng/demotest/util/Md5Service.java
+++ src/main/java/com/yuxinsheng/demotest/util/Md5Service.java
@@ -4,7 +4,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class Md5Service {
     public static void main(String[] args) {
-        String data = "abc";
+        String data = "123";
         System.out.println(DigestUtils.md5Hex(data));
         System.out.println(DigestUtils.md5Hex(data.getBytes()));
     }
